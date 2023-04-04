703e16d8596dcdde7e021c2ac311d57e916ab3b2
Romain <romain.nerot@outlook.fr>
2023-04-04 22:25:47 +0200

    Perfect FPS

--- Assets/Scripts/PhysicalCharacterControllerScript.cs
+++ Assets/Scripts/PhysicalCharacterControllerScript.cs
@@ -1,31 +1,34 @@
-using System;
 using UnityEngine;
 
 public class PhysicalCharacterControllerScript : MonoBehaviour
 {
-    public float jumpForce = 10.0f;
-    public float walkForce = 10.0f; // Realistic
-    public float rotationSpeed = 360f;
-    public float mouseXSensitivity = 1f;
-    public float mouseYSensitivity = 1f;
-    public float maximumHorizontalVelocity = 10f;
-    public Transform characterRootTransform;
-    public Transform characterEyesTransform;
-    public Rigidbody characterRootRigidbody;
-    private float _yawRotationIntent;
-    private float _pitchRotationIntent;
-    private float _forwardMovementIntent;
-    private float _lateralMovementIntent;
-    private bool _wantToJump;
+    public float jumpForce = 10.0f; // Force to apply when jumping
+    public float walkForce = 10.0f; // Force to apply when walking
+    public float rotationSpeed = 360f; // Speed of rotation in degrees per second
+    public float mouseXSensitivity = 1f; // Sensitivity of mouse movement in degrees per pixel
+    public float mouseYSensitivity = 1f; // Sensitivity of mouse movement in degrees per pixel
+    public float maximumHorizontalVelocity = 10f; // Maximum horizontal velocity in meters per second
+    public Transform characterRootTransform; // The transform of the character's root
+    public Transform characterEyesTransform; // The transform of the character's eyes
+    public Rigidbody characterRootRigidbody; // The rigidbody of the character's root
+    private float _yawRotationIntent; // The amount of rotation to apply around the Y axis
+    private float _pitchRotationIntent; // The amount of rotation to apply around the X axis
+    private float _forwardMovementIntent; // The amount of movement to apply forward
+    private float _lateralMovementIntent; // The amount of movement to apply to the side
+    private bool _wantToJump; // Whether the character wants to jump
 
     // Update is called once per frame
     void Update()
     {
+        // Reset the rotation and movement intents
         _yawRotationIntent = 0f;
         _forwardMovementIntent = 0f;
         _lateralMovementIntent = 0f;
         
-        QuakeLikeInput();
+        // Get the input
+        InputManager();
+        
+        // Apply the rotation
         characterRootTransform.Rotate(Vector3.up * (_yawRotationIntent * rotationSpeed * Time.deltaTime));
         characterEyesTransform.Rotate(-Vector3.right * (_pitchRotationIntent * rotationSpeed * Time.deltaTime));
         var localRotation = characterEyesTransform.localEulerAngles.x;
@@ -34,16 +37,18 @@ public class PhysicalCharacterControllerScript : MonoBehaviour
             localRotation -= 360f;
         }
 
+        // Clamp the rotation
         localRotation = Mathf.Clamp(localRotation, -80f, 80f);
         characterEyesTransform.localEulerAngles = new Vector3(localRotation, 0f, 0f);
 
+        // Jump
         if (Input.GetKeyDown(KeyCode.Space))
         {
             _wantToJump = true;
         }
     }
     
-    private void QuakeLikeInput()
+    private void InputManager()
     {
         if (Input.GetKey(KeyCode.Q))
         {
@@ -65,15 +70,18 @@ public class PhysicalCharacterControllerScript : MonoBehaviour
             _forwardMovementIntent += -1f;
         }
 
+        // Get the mouse movement
         _yawRotationIntent = Input.GetAxisRaw("Mouse X") * mouseXSensitivity * Time.deltaTime;
         _pitchRotationIntent = Input.GetAxisRaw("Mouse Y") * mouseYSensitivity * Time.deltaTime;
     }
 
     private void FixedUpdate()
     {
+        // Check if the character is grounded
         if (Physics.SphereCast(characterRootTransform.position + 0.4951f * Vector3.up, 0.495f,
                 Vector3.down, out var hit, 0.1f))
         {
+            // Add a force to the character
             var velocity = characterRootRigidbody.velocity;
             var targetForce = characterRootTransform.forward * _forwardMovementIntent;
             targetForce += characterRootTransform.right * _lateralMovementIntent;
@@ -86,6 +94,7 @@ public class PhysicalCharacterControllerScript : MonoBehaviour
             velocity.y = characterRootRigidbody.velocity.y;
             characterRootRigidbody.velocity = velocity;
             
+            // Jump
             if (_wantToJump)
             {
                 _wantToJump = false;
@@ -96,10 +105,18 @@ public class PhysicalCharacterControllerScript : MonoBehaviour
         }
         else
         {
+            // Apply a friction to the character when in the air
             var velocity = characterRootRigidbody.velocity;
             velocity.x -= velocity.x * 0.02f;
             velocity.z -= velocity.z * 0.02f;
             characterRootRigidbody.velocity = velocity;
+            
+            // Add a air control lower than the ground control
+            var targetForce = characterRootTransform.forward * _forwardMovementIntent;
+            targetForce += characterRootTransform.right * _lateralMovementIntent;
+            targetForce = targetForce.normalized * (walkForce * Time.deltaTime * 0.1f);
+            characterRootRigidbody.AddForce(targetForce,
+                ForceMode.Force);
         }
     }
 }

dc8c73dccf780fd6415a543d572ddb0e505982c4
Romain <romain.nerot@outlook.fr>
2023-04-17 21:45:28 +0200

    Fix traps + power vision

--- Assets/Scripts/UnownScript.cs
+++ Assets/Scripts/UnownScript.cs
@@ -7,20 +7,28 @@ public class UnownScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        var trap = GameObject.FindWithTag("Trap");
+        // Get all the traps
+        var traps = GameObject.FindGameObjectsWithTag("Trap");
+        
         var playerBody = GameObject.FindWithTag("PlayerBody");
         var playerRenderer = playerBody.GetComponent<Renderer>();
         
         if (playerRenderer.material.color != Color.green)
         {
-            // Desactivate the mesh renderer of the trap
-            trap.GetComponent<MeshRenderer>().enabled = false;
+            // Desactivate the mesh renderer of the traps
+            foreach (var trap in traps)
+            {
+                trap.GetComponent<MeshRenderer>().enabled = false;
+            }
         }
         
         if (playerRenderer.material.color == Color.green)
         {
-            // Activate the mesh renderer of the trap
-            trap.GetComponent<MeshRenderer>().enabled = true;
+            // Activate the mesh renderer of the traps
+            foreach (var trap in traps)
+            {
+                trap.GetComponent<MeshRenderer>().enabled = true;
+            }
         }
     }
 }

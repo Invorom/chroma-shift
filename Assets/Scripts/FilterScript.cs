db0e1484a3143dd800a5792c32aa38d6f90d56d5
Romain <romain.nerot@outlook.fr>
2023-04-17 21:54:33 +0200

    17 add power vision (#18)
    * Dev (#16)
    
    * 1 perfect fps (#8)
    
    * Implement FPS
    
    Need to fix:
    - Lil control on fly
    
    * Perfect FPS
    
    * Perfect fps 2.0
    
    * 2 manage mecanics machoke (#9)
    
    * Add machoke strength
    
    * Move cube
    
    * Don't reset jump in the air
    
    * Add metamorph (#11)
    
    * Change player's color (#13)
    
    * Add default material
    
    * Add high jump (#14)
    
    * Unown power (#15)
    
    * Fix traps + power vision

---
+++ Assets/Scripts/FilterScript.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FilterScript : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        var filter = GameObject.FindWithTag("Filter");
+        var playerBody = GameObject.FindWithTag("PlayerBody");
+        var playerRenderer = playerBody.GetComponent<Renderer>();
+        
+        // Get the Filter material
+        var transparentMaterial = filter.GetComponent<MeshRenderer>().material;
+
+        if (playerRenderer.material.color == Color.white)
+        {
+            // Activate the mesh renderer of the filter
+            filter.GetComponent<MeshRenderer>().enabled = false;
+        }
+
+        if (playerRenderer.material.color == Color.red)
+        {
+            // Activate the mesh renderer of the filter
+            filter.GetComponent<MeshRenderer>().enabled = true;
+            // Set the filter material to red transparent
+            transparentMaterial.color = new Color(1, 0, 0, 0.0001f);
+            // Set the filter material emission color to red
+            transparentMaterial.SetColor("_EmissionColor", new Color(1, 0, 0));
+        }
+        
+        if (playerRenderer.material.color == Color.blue)
+        {
+            // Activate the mesh renderer of the filter
+            filter.GetComponent<MeshRenderer>().enabled = true;
+            // Set the filter material to blue transparent
+            transparentMaterial.color = new Color(0, 0, 1, 0.0001f);
+            // Set the filter material emission color to blue
+            transparentMaterial.SetColor("_EmissionColor", new Color(0, 0, 1));
+        }
+        
+        if (playerRenderer.material.color == Color.green)
+        {
+            // Activate the mesh renderer of the filter
+            filter.GetComponent<MeshRenderer>().enabled = true;
+            // Set the filter material to green transparent
+            transparentMaterial.color = new Color(0, 0.6f, 0, 0.0001f);
+            // Set the filter material emission color to green
+            transparentMaterial.SetColor("_EmissionColor", new Color(0, 0.5f, 0));
+        }
+        
+        if (playerRenderer.material.color == Color.magenta)
+        {
+            // Activate the mesh renderer of the filter
+            filter.GetComponent<MeshRenderer>().enabled = true;
+            // Set the filter material to magenta transparent
+            transparentMaterial.color = new Color(1, 0, 1, 0.0001f);
+            // Set the filter material emission color to magenta
+            transparentMaterial.SetColor("_EmissionColor", new Color(0.7f, 0, 0.7f));
+        }
+    }
+}

a01cb0ff305f35e0f5f6f193b67231e881384c4b
johan <johanribl@gmail.com>
2023-04-24 21:51:50 +0200

    Start add btn

---
+++ Assets/Scripts/OpenDoor.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour
+{
+    
+    public GameObject leftDoor;
+    public GameObject rightDoor;
+    public GameObject btn;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        btn.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

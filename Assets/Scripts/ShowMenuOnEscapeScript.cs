29e8e31c2c94e18976056e948332fa14319b895f
johan <johanribl@gmail.com>
2023-04-26 00:54:57 +0200

    Ui ingame

---
+++ Assets/Scripts/ShowMenuOnEscapeScript.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowMenuOnEscapeScript : MonoBehaviour
+{
+    public GameObject panel;
+    public GameObject player;
+    public GameObject ScriptHolder;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (panel.activeSelf == false)
+            {
+                Cursor.visible = true;
+                Cursor.lockState = CursorLockMode.None;
+                // disable all scripts on player
+                var scripts = player.GetComponents<MonoBehaviour>();
+                foreach (var script in scripts)
+                {
+                    script.enabled = false;
+                }
+                // disable hide cursor script on ScriptHolder
+                var hideCursorScript = ScriptHolder.GetComponent<HideCursorScript>();
+                hideCursorScript.enabled = false;
+                // enable panel
+                panel.SetActive(true);
+            }
+            else
+            {
+                Cursor.visible = false;
+                Cursor.lockState = CursorLockMode.Locked;
+                // enable all scripts on player
+                var scripts = player.GetComponents<MonoBehaviour>();
+                foreach (var script in scripts)
+                {
+                    script.enabled = true;
+                }
+                // enable hide cursor script on ScriptHolder
+                var hideCursorScript = ScriptHolder.GetComponent<HideCursorScript>();
+                hideCursorScript.enabled = true;
+                // disable panel
+                panel.SetActive(false);
+            }
+        }
+    }
+}

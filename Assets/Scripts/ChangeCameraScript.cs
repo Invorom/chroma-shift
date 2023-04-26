7193fa031591f472c25ae7df587f437891437e79
Romain <romain.nerot@outlook.fr>
2023-04-26 02:34:40 +0200

    22 puzzle make the first room (#29)
    * Add some ice
    
    * Add decorations
    
    * Add design + mechanic
    
    * Import adam
    
    * Design
    
    * Add candles
    
    * Add grill
    
    * Add grill
    
    * Fix grill
    
    * Fix grill
    
    * Change player position
    
    * Fix
    
    * Add digicode
    
    * Add rayCast on leftMouseClick
    
    * Add crosshair
    
    * Add rayCast + digicode interaction
    
    * Digicode works
    
    * Add screen and camera
    
    * Add adam
    
    * Add new buildings
    
    * Add plateform
    
    * Add humanoids
    
    * advance
    
    * Fix button
    
    * Make difference between Digicode and button
    
    * Add lamp + camera texture
    
    * Change code + door
    
    * Add lamps
    
    * Fix Ice
    
    * Add the end
    
    * Details
    
    * Add End
    
    * Details

---
+++ Assets/Scripts/ChangeCameraScript.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class ChangeCameraScript : MonoBehaviour
+{
+    public RenderTexture RenderTexture;
+    public RenderTexture RedRenderTexture;
+    public RenderTexture BlueRenderTexture;
+    public RenderTexture GreenRenderTexture;
+    public RenderTexture YellowRenderTexture;
+    public RenderTexture BlackRenderTexture;
+    public Collider Button;
+    
+    void Update()
+    {
+        var player = GameObject.FindWithTag("Player");
+        var playerBody = GameObject.FindWithTag("PlayerBody");
+        var eyes = GameObject.FindWithTag("Eyes");
+        var eyesCamera = eyes.GetComponent<Camera>();
+        var screen = GameObject.FindWithTag("Screen");
+        var screenRenderer = screen.GetComponent<Renderer>();
+
+        // Get my own texture
+        var whiteMaterial = Resources.Load("Test", typeof(Material)) as Material;
+
+        if (Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            RaycastHit hit;
+            Ray ray = eyesCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+            // If the raycast hit Button tag object (Digicode)
+            if (Physics.Raycast(ray, out hit, 2.0f))
+            {
+                if (hit.collider == Button)
+                {
+                    if (screenRenderer.material.mainTexture == RenderTexture)
+                    {
+                        screenRenderer.material.mainTexture = RedRenderTexture;
+                    }
+                    else if (screenRenderer.material.mainTexture == RedRenderTexture)
+                    {
+                        screenRenderer.material.mainTexture = BlueRenderTexture;
+                    }
+                    else if (screenRenderer.material.mainTexture == BlueRenderTexture)
+                    {
+                        screenRenderer.material.mainTexture = GreenRenderTexture;
+                    }
+                    else if (screenRenderer.material.mainTexture == GreenRenderTexture)
+                    {
+                        screenRenderer.material.mainTexture = YellowRenderTexture;
+                    }
+                    else if (screenRenderer.material.mainTexture == YellowRenderTexture)
+                    {
+                        screenRenderer.material.mainTexture = BlackRenderTexture;
+                    }
+                    else if (screenRenderer.material.mainTexture == BlackRenderTexture)
+                    {
+                        screenRenderer.material.mainTexture = RenderTexture;
+                    }
+                }
+            }
+        }
+    }
+}
